5717c6d516575fbb5ac44f670667840c9b22565d
Yusren270 <usreniso@gmail.com>
2024-12-13 09:04:54 +0200

    Add files via upload

---
+++ faktorel/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faktorel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var n = int.Parse(Console.ReadLine());
+            var P = 1;
+            do
+            {
+                P = n * P;
+                n--;
+
+            } 
+            while (n > 1);
+            Console.WriteLine(P);
+           
+
+
+
+
+        }
+    }
+}
