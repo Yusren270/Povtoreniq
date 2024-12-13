6aae7f9d9cfcaaff723ca4d4cb9bd3442063756c
Yusren270 <usreniso@gmail.com>
2024-12-13 09:07:50 +0200

    Add files via upload

---
+++ Hokin/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Hokin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+            Console.Write("vuvedi znak +,-,*,/");
+            var c = Console.ReadLine();
+            switch (c)
+            {
+                case "+":
+                    Console.WriteLine("a+b=" +( a+b));break;
+                case "-":
+                    Console.WriteLine("a-b=" +(a - b));break;
+                case "*":
+                    Console.WriteLine("a*b=" + (a * b));break;
+                 case "/":
+                        Console.WriteLine("a/b="+(a/b));break;
+            }
+
+
+
+
+
+        }
+    }
+}
